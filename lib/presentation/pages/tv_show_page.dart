4136ec2d244f36b99812e456b4f55f61909b54fa
Raptor200111 <alvardalsva@gmail.com>
2026-01-14 11:18:10 +0100

    TvShow Page with Slivers

--- lib/presentation/pages/tv_show_page.dart
+++ lib/presentation/pages/tv_show_page.dart
@@ -14,100 +14,94 @@ class TvShowPage extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
     
-    final widgetWidth = screenWidth * 0.9;
-
+    Image backdropImage = tvShow.backdropPath.isNotEmpty
+        ? Image.network(
+            'https://image.tmdb.org/t/p/w500${tvShow.backdropPath}',
+            fit: BoxFit.cover,
+          )
+        : Container(
+            color: Colors.grey[300],
+            width: screenWidth,
+            child: Icon(Icons.tv, size: screenWidth * 0.1),
+          ) as Image;
+    
     return Scaffold(
-        backgroundColor: Colors.white,
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => Navigator.pop(context),
+      extendBodyBehindAppBar: true,
+      body: CustomScrollView(
+
+        slivers: [
+          SliverAppBar(
+            automaticallyImplyLeading: true,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.grey),
+              onPressed: () => Navigator.pop(context),
+            ),
+            backgroundColor: Colors.transparent,
+            expandedHeight: screenHeight * 0.25,
+            //toolbarHeight: 150,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: backdropImage,
+            ),
+            elevation: 0,
           ),
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        body: Column(
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Stack(
-                      clipBehavior: Clip.none,
+                    Padding(
+                      padding: EdgeInsets.all(screenWidth * 0.05),
+                      child: Row(
                       children: [
                         ClipRRect(
-                          child: tvShow.backdropPath.isNotEmpty
+                          borderRadius: BorderRadius.circular(8),
+                          child: tvShow.posterPath.isNotEmpty
                               ? Image.network(
-                                  'https://image.tmdb.org/t/p/w500${tvShow.backdropPath}',
+                                  'https://image.tmdb.org/t/p/w200${tvShow.posterPath}',
+                                  width: screenWidth * 0.4,
+                                  //height: 260,
                                   fit: BoxFit.cover,
-                                  width: screenWidth,
-                                  //height: 200,
                                 )
                               : Container(
                                   color: Colors.grey[300],
-                                  width: screenWidth,
-                                  //height: 200,
-                                  child: Icon(Icons.tv, size: widgetWidth * 0.1),
+                                  width: screenWidth * 0.5,
+                                  height: 260,
+                                  child: Icon(Icons.tv, size: screenWidth * 0.1),
                                 ),
                         ),
-                        Positioned(
-                          left: screenWidth * 0.05,
-                          bottom: screenHeight * -0.25,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.3),
-                                  blurRadius: 12,
-                                  offset: const Offset(0, 8),
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.only(left: screenWidth * 0.05),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  tvShow.name,
+                                  //maxLines: 2,
+                                  overflow: TextOverflow.visible,
+                                  style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                                 ),
+                              const SizedBox(height: 8),
+                              RatingWidget(tvShow: tvShow),
+                              const SizedBox(height: 8),
+                              Text(
+                                "Popularity: ${tvShow.popularity.toStringAsFixed(1)}",
+                                style: const TextStyle(fontSize: 14, color: Colors.grey),
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                "First Air Date: ${tvShow.firstAirDate}",
+                                style: const TextStyle(fontSize: 14, color: Colors.grey),
+                              ),
                               ],
                             ),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(8),
-                              child: tvShow.posterPath.isNotEmpty
-                                  ? Image.network(
-                                      'https://image.tmdb.org/t/p/w200${tvShow.posterPath}',
-                                      width: screenWidth * 0.4,
-                                      fit: BoxFit.cover,
-                                      //height: 260,
-                                    )
-                                  : Container(
-                                      color: Colors.grey[300],
-                                      width: screenWidth * 0.5,
-                                      height: 260,
-                                      child: Icon(Icons.tv, size: widgetWidth * 0.1),
-                                    ),
-                            ),
                           ),
                         ),
-                      ],
+                      ]
                     ),
-                    Padding(
-                      padding: EdgeInsets.only(left: screenWidth * 0.5, right: 0, top: screenWidth * 0.025, bottom: screenWidth * 0.05),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            tvShow.name,
-                            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(height: 8),
-                          RatingWidget(tvShow: tvShow),
-                          const SizedBox(height: 8),
-                          Text(
-                            "Popularity: ${tvShow.popularity.toStringAsFixed(1)}",
-                            style: const TextStyle(fontSize: 14, color: Colors.grey),
-                          ),
-                          const SizedBox(height: 4),
-                          Text(
-                            "First Air Date: ${tvShow.firstAirDate}",
-                            style: const TextStyle(fontSize: 14, color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: screenHeight * 0.10),
+                  ),
                     GenreGridWidget(tvShow: tvShow, genres: genres),
                     Padding(padding:  EdgeInsets.all(screenWidth * 0.05),
                       child: Text(
@@ -115,12 +109,27 @@ class TvShowPage extends StatelessWidget {
                         style: const TextStyle(fontSize: 16),
                       ),
                     ),
-                      Text("Origin Language: ${tvShow.originalLanguage}", textAlign: TextAlign.left),
-                      Text("Original Name: ${tvShow.originalName}", textAlign: TextAlign.left),
-                      Text("Origin Country: ${tvShow.originCountry.join(', ')}", textAlign: TextAlign.left),
-                      
+                    Padding(padding:  EdgeInsets.only(left: screenWidth * 0.05, right: screenWidth * 0.05, bottom: screenWidth * 0.05),
+                      child: Row(
+                        children:[
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Original Name: ${tvShow.originalName}", maxLines: null, overflow: TextOverflow.clip),
+                                Text("Origin Language: ${tvShow.originalLanguage}", maxLines: null, overflow: TextOverflow.clip),
+                                Text("Origin Country: ${tvShow.originCountry.join(', ')}", maxLines: null, overflow: TextOverflow.clip),
+                                //Text("Original Name: ${tvShow.originalName}\nOrigin Language: ${tvShow.originalLanguage}", maxLines: null, overflow: TextOverflow.clip)
+                              ],
+                            ),
+                          ),
+                        ]
+                      ),)
                     ],
                   ),
-        );
-    }
+          )
+        ],
+      )
+    );
   }
+}
